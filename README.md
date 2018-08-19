800da62d3a30805fae1241d675cea60da836d565
冉光宇 <ranguangyu@zbj.com>
2018-08-19 18:57:05 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # rget-cli
-rget-cli 初始化自定义项目
+cli for rget
+
+## Version
+```shell
+node -v
+#v8.9.3 up
+npm -v
+#5.6.0 up
+```
+
+## Installation
+
+Using npm:
+
+```shell
+npm install -g rget-cli
+
+## 初始化
+goto init <project name>
+
+## 指定仓库初始化
+goto init <project name> -r https://github.com:rgy19930329/kyvue-template#master
+```
---
+++ test/test-inquirer.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer')
+
+inquirer.prompt([{
+  name: 'projectName',
+  message: '项目的名称',
+  default: 'tt'
+}, {
+  name: 'projectVersion',
+  message: '项目的版本号',
+  default: '1.0.0'
+}, {
+  name: 'projectDescription',
+  message: '项目的简介',
+  default: `A project named kylinjs`
+}]).then(answers => {
+  console.log(answers);
+});
